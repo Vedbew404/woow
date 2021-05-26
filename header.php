ef4c9a2f443210a8edb7849c5674bc129ddc4d79
benfran21 <benfran21@hotmail.com>
2021-05-25 20:27:19 -0400

    corección
    Corección de la ruta en style.css

--- header.php
+++ header.php
@@ -36,7 +36,7 @@
 	</title>
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous">
 	<!-- <link rel="stylesheet" href="css/bootstrap.min.css" crossorigin="anonymous">	 -->
-	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="./wp-content/plugins/um_vdbw404/css/style.css">
 
 	<?php wp_head(); ?>
 </head>
