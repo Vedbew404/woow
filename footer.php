f2bd06aa99229fd734341aa255bcf75e3f57686e
benfran21 <benfran21@hotmail.com>
2021-05-25 20:39:54 -0400

    Correccion
    Correccion en la linea 58

--- footer.php
+++ footer.php
@@ -54,11 +54,16 @@
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"
 		integrity="sha384-p34f1UUtsS3wqzfto5wAAmdvj+osOnFyQFpp4Ua3gs/ZVWx6oOypYoCJhGGScy+8" crossorigin="anonymous">
 	</script>
-</body>
 
 <?php wp_footer(); ?>
 
+</body>
+
 </html>
+
+
+
+
 <footer id="colophon" class="site-footer">
     <div class="site-info">
         <a href="<?php echo esc_url( __( 'https://wordpress.org/', 'woow' ) ); ?>">
