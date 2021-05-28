5e2dc225947ac836fc573ec3dd321d62765fdce7
benfran21 <benfran21@hotmail.com>
2021-05-28 11:15:53 -0400

    Cambios en rutas y estructuras

--- footer.php
+++ footer.php
@@ -10,64 +10,56 @@
  */
 
 ?>
-		<footer id="colophon" class="site-footer">
-			<div id="prefooter">
-				<div class="container-fluid">
-					<div class="row">
-						<div class="right col-sm-2">
-							<a href="">NOSOTROS</a>
-						</div>
-						<div class="col-sm-3">
-							<a href="">REGISTRO MASCOTAS</a>
-						</div>
-						<div class="col-sm-2">
-							<a href="">CAMBIOS Y DEVOLUCIONES</a>
-						</div>
-						<div class="col-sm-3">
-							<a href="">POLÍTICA DE PRIVACIDAD</a>
-						</div>
-						<div class="left col-sm-2">
-							<a href="">CONTACTAR UN ASESOR</a>
-						</div>
-						<div>
-							<p>Copyright 2021 © WOOW UNIVERSO ANIMAL SAS NIT 000000000-0 Todos los derechos
-								reservados.
-							</p>
-						</div>
-					</div>
-				</div>
-			</div>
-			<div id="footer">
-				<p>SITIO WEB BYGREGORIRAMIREZ / CONSTRUCCIÓN 4VEW</p>
-			</div>
-		</footer><!-- #colophon -->
-	</div><!-- #page -->
-	<!-- <script src="js/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
+<footer id="colophon" class="site-footer">
+    <div id="prefooter">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="right col-sm-2">
+                    <a href="">NOSOTROS</a>
+                </div>
+                <div class="col-sm-3">
+                    <a href="">REGISTRO MASCOTAS</a>
+                </div>
+                <div class="col-sm-2">
+                    <a href="">CAMBIOS Y DEVOLUCIONES</a>
+                </div>
+                <div class="col-sm-3">
+                    <a href="">POLÍTICA DE PRIVACIDAD</a>
+                </div>
+                <div class="left col-sm-2">
+                    <a href="">CONTACTAR UN ASESOR</a>
+                </div>
+                <div>
+                    <p>Copyright 2021 © WOOW UNIVERSO ANIMAL SAS NIT 000000000-0 Todos los derechos
+                        reservados.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div id="footer">
+        <p>SITIO WEB BYGREGORIRAMIREZ / CONSTRUCCIÓN 4VEW</p>
+    </div>
+</footer><!-- #colophon -->
+</div><!-- #page -->
+<!-- <script src="js/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
 		crossorigin="anonymous">
 	</script>
 	<script src="js/bootstrap.min.js" integrity="sha384-oesi62hOLfzrys4LxRF63OJCXdXDipiYWBnvTl9Y9/TRlw5xlKIEHpNyvvDShgf/"
 		crossorigin="anonymous">
 	</script> -->
-	<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js"
-		integrity="sha384-SR1sx49pcuLnqZUnnPwx6FCym0wLsk5JZuNx2bPPENzswTNFaQU1RDvt3wT4gWFG" crossorigin="anonymous">
-	</script>
-	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"
-		integrity="sha384-p34f1UUtsS3wqzfto5wAAmdvj+osOnFyQFpp4Ua3gs/ZVWx6oOypYoCJhGGScy+8" crossorigin="anonymous">
-	</script>
-
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js"
+    integrity="sha384-SR1sx49pcuLnqZUnnPwx6FCym0wLsk5JZuNx2bPPENzswTNFaQU1RDvt3wT4gWFG" crossorigin="anonymous">
+</script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-p34f1UUtsS3wqzfto5wAAmdvj+osOnFyQFpp4Ua3gs/ZVWx6oOypYoCJhGGScy+8" crossorigin="anonymous">
+</script>
 <?php wp_footer(); ?>
-
 </body>
-
-</html>
-
-
-
-
-<footer id="colophon" class="site-footer">
+<!-- <footer id="colophon" class="site-footer">
     <div class="site-info">
         <a href="<?php echo esc_url( __( 'https://wordpress.org/', 'woow' ) ); ?>">
-            <?php
+		<?php
 				/* translators: %s: CMS name, i.e. WordPress. */
 				printf( esc_html__( 'Proudly powered by %s', 'woow' ), 'WordPress' );
 				?>
@@ -78,11 +70,7 @@
 				printf( esc_html__( 'Theme: %1$s by %2$s.', 'woow' ), 'woow', '<a href="http://woow.me/">woow.me</a>' );
 				?>
     </div><!-- .site-info -->
-</footer><!-- #colophon -->
-</div><!-- #page -->
-
-<?php wp_footer(); ?>
-
-</body>
+<!--</footer><!-- #colophon -->
+<!--</div> <!-- #page -->
 
 </html>
--- header.php
+++ header.php
@@ -20,38 +20,38 @@
     <meta name="description" content="WOOW">
     <meta name="keywords" content="Cuidado de tus mascotas, Rescate animal, Busqueda por GPS">
     <meta name="author" content="Francisco González">
-    <link rel="apple-touch-icon" sizes="57x57" href="./wp-content/plugins/um_vdbw404/img/apple-icon-57x57.png">
-    <link rel="apple-touch-icon" sizes="60x60" href="./wp-content/plugins/um_vdbw404/img/apple-icon-60x60.png">
-    <link rel="apple-touch-icon" sizes="72x72" href="./wp-content/plugins/um_vdbw404/img/apple-icon-72x72.png">
-    <link rel="apple-touch-icon" sizes="76x76" href="./wp-content/plugins/um_vdbw404/img/apple-icon-76x76.png">
-    <link rel="apple-touch-icon" sizes="114x114" href="./wp-content/plugins/um_vdbw404/img/apple-icon-114x114.png">
-    <link rel="apple-touch-icon" sizes="120x120" href="./wp-content/plugins/um_vdbw404/img/apple-icon-120x120.png">
-    <link rel="apple-touch-icon" sizes="144x144" href="./wp-content/plugins/um_vdbw404/img/apple-icon-144x144.png">
-    <link rel="apple-touch-icon" sizes="152x152" href="./wp-content/plugins/um_vdbw404/img/apple-icon-152x152.png">
-    <link rel="apple-touch-icon" sizes="180x180" href="./wp-content/plugins/um_vdbw404/img/apple-icon-180x180.png">
-    <link rel="icon" type="image/png" sizes="192x192" href="./wp-content/plugins/um_vdbw404/img/android-icon-192x192.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="./wp-content/plugins/um_vdbw404/img/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="96x96" href="./wp-content/plugins/um_vdbw404/img/favicon-96x96.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="./wp-content/plugins/um_vdbw404/img/favicon-16x16.png">
+    <link rel="apple-touch-icon" sizes="57x57" href="../wp-content/plugins/um_vdbw404/img/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="../wp-content/plugins/um_vdbw404/img/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="../wp-content/plugins/um_vdbw404/img/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="../wp-content/plugins/um_vdbw404/img/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="../wp-content/plugins/um_vdbw404/img/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="../wp-content/plugins/um_vdbw404/img/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="../wp-content/plugins/um_vdbw404/img/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="../wp-content/plugins/um_vdbw404/img/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="../wp-content/plugins/um_vdbw404/img/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192" href="../wp-content/plugins/um_vdbw404/img/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="../wp-content/plugins/um_vdbw404/img/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="../wp-content/plugins/um_vdbw404/img/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="../wp-content/plugins/um_vdbw404/img/favicon-16x16.png">
     <title>WOOW | Bienvenidos
     </title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous">
     <!-- <link rel="stylesheet" href="css/bootstrap.min.css" crossorigin="anonymous">	 -->
-    <link rel="stylesheet" href="./wp-content/themes/woow/css/style.css">
+    <link rel="stylesheet" href="../wp-content/themes/woow/css/style.css">
 
     <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
     <?php wp_body_open(); ?>
-    <div id="page" class="site">
+    <!-- <div id="page" class="site">
         <a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'woow' ); ?></a>
-        <header id="masthead" class="site-header">
-            <div class="site-branding">
+        <header id="masthead" class="site-header"> -->
+            <!-- <div class="site-branding">
                 <?php
 			// the_custom_logo();
-			if ( is_front_page() && is_home() ) :
+			/*if ( is_front_page() && is_home() ) :
 				?>
                 <h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>"
                         rel="home"><?php bloginfo( 'name' ); ?></a></h1>
@@ -68,10 +68,10 @@
                 <p class="site-description">
                     <?php echo $woow_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
                 </p>
-                <?php endif; ?>
-            </div><!-- .site-branding -->
+                <?php endif;*/ ?>
+            </div>.site-branding -->
 
-            <nav id="site-navigation" class="main-navigation">
+            <!-- <nav id="site-navigation" class="main-navigation">
                 <button class="menu-toggle" aria-controls="primary-menu"
                     aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'woow' ); ?></button>
                 <?php
@@ -82,5 +82,5 @@
 				)
 			);
 			?>
-            </nav><!-- #site-navigation -->
+            </nav>#site-navigation -->
         </header><!-- #masthead -->
