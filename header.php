5ea20db1e6bb30545f3146b7253b480abc2843b5
benfran21 <benfran21@hotmail.com>
2021-05-31 12:19:11 -0400

    Actualizacion
    hoja de estilos unificada con la principal de wordpress

--- header.php
+++ header.php
@@ -39,7 +39,6 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous">
     <!-- <link rel="stylesheet" href="css/bootstrap.min.css" crossorigin="anonymous">	 -->
-    <link rel="stylesheet" href="../wp-content/themes/woow/css/style.css">
 
     <?php wp_head(); ?>
 </head>
